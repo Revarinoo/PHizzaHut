af4fb6b2085697fb050bb83207d21cde75e27c62
ArdiDramilio <ardidramilio83@gmail.com>
2020-11-30 21:14:57 +0700

    loginAndHome(3)

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -5,19 +5,20 @@
         <h1>Our Pizza</h1>
     </div>
     <div class="container">
-        @foreach ($pizzas as $pizza)
-        <div class="card d-inline-block">
-            <div class="imgwrap">
-                <img class="card-img-top" src="/images/{{$pizza->image}}" alt="Card image cap">
+        <div class="row justify-content-center">
+            @foreach ($pizzas as $pizza)
+            <div class="card d-inline-block ml-2 mt-2 mb-3 mr-2" style="width:20rem;">
+                <div class="imgwrap">
+                    <img class="card-img-top" src="/images/{{$pizza->image}}" alt="Card image cap">
+                </div>
+                <div class="card-body">
+                <h5 class="card-title text-center font-weight-bold">{{$pizza->name}}</h5>
+                <h5 class="card-title text-center font-weight-bold">{{$pizza->price}}</h5>
+                </div>
             </div>
-            <div class="card-body">
-            <h5 class="card-title text-center font-weight-bold">{{$pizza->name}}</h5>
-            <h5 class="card-title text-center font-weight-bold">{{$pizza->price}}</h5>
-            </div>
+            @endforeach 
         </div>
-        @endforeach 
         {{$pizzas->links()}}
     </div>
     
-
 @endsection
