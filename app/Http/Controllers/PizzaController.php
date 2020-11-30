c9d19ef693e2cdad87eb86adcffb8833c2af861e
ArdiDramilio <ardidramilio83@gmail.com>
2020-11-30 20:51:00 +0700

    loginAndHome(1)

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Pizza;
 class PizzaController extends Controller
 {
     /**
@@ -13,7 +13,8 @@ class PizzaController extends Controller
      */
     public function index()
     {
-        return view('pizza.index');
+        $pizzas = Pizza::paginate(6);
+        return view('pizza.index',compact('pizzas'));
     }
 
     /**
--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -2,8 +2,22 @@
 
 @section('content')
     <div class="pl-5">
-        <h1 class="container">Login Form</h1>
-        <h1>Testt Login Form</h1>
+        <h1>Our Pizza</h1>
     </div>
+    <div class="container">
+        @foreach ($pizzas as $pizza)
+        <div class="card d-inline-block">
+            <div class="imgwrap">
+                <img class="card-img-top" src="/images/{{$pizza->image}}" alt="Card image cap">
+            </div>
+            <div class="card-body">
+            <h5 class="card-title text-center font-weight-bold">{{$pizza->name}}</h5>
+            <h5 class="card-title text-center font-weight-bold">{{$pizza->price}}</h5>
+            </div>
+        </div>
+        @endforeach 
+        {{$pizzas->links()}}
+    </div>
+    
 
 @endsection
