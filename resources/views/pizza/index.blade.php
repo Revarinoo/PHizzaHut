37611c3c7b15ca562762e3cd2fc2cc1dd31a3f7f
ArdiDramilio <ardidramilio83@gmail.com>
2020-11-30 16:03:28 +0700

    Test2

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -2,12 +2,8 @@
 
 @section('content')
     <div class="pl-5">
-<<<<<<< HEAD
         <h1 class="container">Login Form</h1>
         <h1>Testt Login Form</h1>
-=======
-        <h1 class="container">Pizza</h1>
->>>>>>> f5fd0bfe3a97469d23937996ba965653f3cddadb
     </div>
 
 @endsection
