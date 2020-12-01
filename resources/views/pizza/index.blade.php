47365559de2028725945121183cabff1f08f3ded
ArdiDramilio <ardidramilio83@gmail.com>
2020-12-01 13:07:44 +0700

    fiture searching

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="pl-5">
-        <h1>Our Pizza</h1>
-    </div>
     <div class="container">
+        
+        <div class="title">
+            <h1 class="text-dark mt-3">Our Delicious Pizza!</h1>
+        </div>
+
+        <form method="get">
+            <h4 class="text-dark ml-5">Search Pizza: </h4>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control ml-5" placeholder="Pizza's Name" name="pizzaname" aria-label="Pizza's Name" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary ml-3 mr-5" type="submit">Search</button>
+                </div>
+            </div>
+        </form>
+
         <div class="row justify-content-center">
+            @if ($pizzas->count() ==0)
+                <h5>Pizza Not Found</h5>
+            @endif
             @foreach ($pizzas as $pizza)
             <div class="card d-inline-block ml-2 mt-2 mb-3 mr-2" style="width:20rem;">
                 <div class="imgwrap">
